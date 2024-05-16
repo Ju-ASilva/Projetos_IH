d13d559373df8d8c4693cea35356815623a51589
Juliana Luiza de Andrade e Silva <jlas2@WINDOWS.CIN.UFPE.BR>
2024-05-16 12:08:14 -0300

    Questão pronta

--- Ficha01/Q1_a.asm
+++ Ficha01/Q1_a.asm
@@ -2,14 +2,29 @@
 lw x9, a
 lw x18, b
 lw x19, c
-li x20, x
+lw x20, x
 
-blt x9, 5, final # a < 5
-bge x18, 66, final #b >= 66
-blt 15, x19, final #15 < c
+add x20, x0, x0
+addi x21, x0, 5
+addi x22, x0, 66
+addi x23, x0, 15
 
-final: 
-	halt 
+blt x9, x21, final1 # a < 5
+bge x18, x22, final2 #b >= 66
+blt x19, x23, final3 #c < 15
+
+addi x20, x0, 1
+halt 
+
+final1:
+	addi x25, x0, 8
+	halt
+final2:
+	addi x26, x0, 8
+	halt
+final3:
+	addi x27, x0, 8 
+	halt
 
 a: .word 6
 b: .word 6
